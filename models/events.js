062f7e40d3addd1480317b93f4da99e5c024909f
Vernon Lillies <vernon.lillies@gmail.com>
2018-02-06 00:03:44 -0500

    feat: add uuid generator and id field to events model

--- models/events.js
+++ models/events.js
@@ -1,5 +1,8 @@
+const uuidv4 = require('uuid/v4')
+
 class Event {
   constructor (title, patientName, duration) {
+    this.id = uuidv4()
     this.createdDate = new Date()
     this.title = title
     this.patientName = patientName
