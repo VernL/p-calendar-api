f1bf7ca56e5caa30a514252fb141ff5bc00ecfe3
Vernon Lillies <vernon.lillies@gmail.com>
2018-02-05 22:18:30 -0500

    feat: add placeholder CRUD routes to the events route

--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+//list
 router.get('/', function(req, res, next) {
-  res.send('Welcome to the events route!');
+  res.send('This will be used to list all events');
+});
+
+//add
+router.post('/', function(req, res, next) {
+  res.send('This will be used to add a new event');
+});
+
+//view
+router.get('/:id', function(req, res, next) {
+  res.send('This will be used to view a specific event');
+});
+
+//edit
+router.put('/:id', function(req, res, next) {
+  res.send('This will be used to edit an event');
+});
+
+//remove
+router.delete('/:id', function(req, res, next) {
+  res.send('This will be used to remove an event');
 });
 
 module.exports = router;
