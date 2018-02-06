e3bb5a8b63d3f5467455934797d306496a07f491
Vernon Lillies <vernon.lillies@gmail.com>
2018-02-06 11:23:52 -0500

    feat: add momentjs package to manage dates in event object

--- models/event.js
+++ models/event.js
@@ -1,12 +1,16 @@
 const uuidv4 = require('uuid/v4');
+const moment = require('moment')
+
+moment().format()
 
 class Event {
   constructor (title, patientName, duration) {
     this.id = uuidv4()
-    this.createdDate = new Date()
+    this.createdDate = moment()
     this.title = title
     this.patientName = patientName
     this.duration = duration
+    this.completionDate = moment(this.createdDate).add(this.duration, 'days').format('LLLL')
   }
 }
 
